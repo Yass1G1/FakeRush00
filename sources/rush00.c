4cb02fcc3268d0fbab92d0e400ec724075eea8c1
Yassine Chouiti <ychouiti@student.42berlin.de>
2023-07-23 14:32:08 +0200

    changing to more logical functions name

--- sources/rush00.c
+++ sources/rush00.c
@@ -6,74 +6,63 @@
 /*   By: ychouiti <ychouiti@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 15:43:52 by ychouiti          #+#    #+#             */
-/*   Updated: 2023/07/22 16:50:10 by ychouiti         ###   ########.fr       */
+/*   Updated: 2023/07/22 18:30:40 by ychouiti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	largeur(int x)
+void	v_line(int x)
 {
 	int	width;
 
 	width = x;
-
-    // ---------- CORNERS ----------
-	if (width == x || width == 1) 
+	if (width == x || width == 1)
 	{
 		ft_putchar('o');
-		width--; 
-
-                // --------- LARGEUR ---------- 
-                while (width > 1)
-                {
-                        ft_putchar('-');
-                        width--;
-				}
-
-                ft_putchar('o');
-                width--;
-                ft_putchar('\n');
-                
+		width--;
+		while (width > 1)
+		{
+			ft_putchar('-');
+			width--;
+		}
+		ft_putchar('o');
+		width--;
+		ft_putchar('\n');
 	}
 }
 
-void	hauteur(int x, int y)
+void	h_line(int x, int y)
 {
-		int	width;
-		int	height;
- 
-		height = y;
+	int	width;
+	int	height;
 
-	// ---------- HAUTEUR -----------
-		while (height > 1)
+	height = y;
+	while (height > 1)
+	{
+		width = x;
+		ft_putchar('|');
+		width--;
+		while (width > 1)
 		{
-			width = x;
-			ft_putchar('|');
+			ft_putchar(' ');
 			width--;
-			
-			while (width > 1)
-			{
-				ft_putchar(' ');
-				width--;
-			}
-			ft_putchar('|');
-			ft_putchar('\n');
-			height--;
 		}
+		ft_putchar('|');
+		ft_putchar('\n');
+		height--;
+	}
 }
 
-void	h_line(y)
+void	h_1_line(int y)
 {
-	int counter = y;
-
 	ft_putchar('o');
 	ft_putchar('\n');
-	while (counter > 2)
+	while (y > 2)
 	{
 		ft_putchar('|');
 		ft_putchar('\n');
-		counter--;
+		y--;
 	}
 	ft_putchar('o');
 }
@@ -83,29 +72,25 @@ void	rush(int x, int y)
 	int	width;
 	int	height;
 
-	width = x; // on déclare que la largeur parce que la hauteur sera déclaré dans une boucle
+	width = x;
 	height = y;
-
 	if (x == 1 && y == 1)
-	{
 		ft_putchar('o');
-	}
 	else if (x == 1 || y == 1)
 	{
 		if (x == 1)
 		{
-			h_line(y);	
+			h_1_line(y);
 		}
 		else
 		{
-			largeur(x);
+			v_line(x);
 		}
 	}
 	else
 	{
-		largeur(x);
-		hauteur(x, y);
-		largeur(x);
+		v_line(x);
+		h_line(x, y);
+		v_line(x);
 	}
-	
 }
